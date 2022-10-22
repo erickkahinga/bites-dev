23ead2a15136e4e983a3e69405ffc67d590a046d
MBronars <mattbronars@berkeley.edu>
2022-10-22 08:14:26 -0400

    adding assets

--- bites/bites_frontend/src/components/Login.jsx
+++ bites/bites_frontend/src/components/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import GoogleLogin from 'react-google-login';
 import {useNavigate} from 'react-router-dom';
 import {FcGoogle} from 'react-icons/fc';
+import shareVideo from '../assets/sleepy.mp4'
+import logo from '../assets/carrot.png'
 
 const Login = () => {
   return (
