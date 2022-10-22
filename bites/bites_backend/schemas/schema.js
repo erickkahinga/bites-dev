0e9ed924830eed57d50d5414e73483350e55e7b8
MBronars <mattbronars@berkeley.edu>
2022-10-22 11:30:17 -0400

    feat: new sanity schema objects

--- bites/bites_backend/schemas/schema.js
+++ bites/bites_backend/schemas/schema.js
@@ -1,9 +1,16 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import pin from './pin';
+import user from './user';
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-import user from './user'
+import restaurant from './restaurant'
+import menuItem from './menuItem'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -13,6 +20,8 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    user
+    user,
+    restaurant,
+    menuItem
   ]),
 })
--- bites/bites_backend/schemas/user.js
+++ bites/bites_backend/schemas/user.js
@@ -1,4 +1,5 @@
-export default{
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
     name: 'user',
     title: 'User',
     type: 'document',
@@ -10,8 +11,26 @@ export default{
         },
         {
             name: 'image',
-            title: 'image',
+            title: 'Image',
             type: 'string'
+        },
+        {
+            name: 'followedRestuarants',
+            title: 'FollowedRestaurants',
+            type: 'array',
+            of: [{type: 
+                'reference',
+                to: [{type: 'restaurant'}]
+            }]
+        },
+        {
+            name: 'followedUsers',
+            title: 'FollowedUsers',
+            type: 'array',
+            of: [{type: 
+                'reference',
+                to: [{type: 'user'}]
+            }]
         }
-    ]
-}
+    ],
+};
